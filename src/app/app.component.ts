3dcd0a831fafa8eae69c9ceff208c6ac9bd35615
TInjusan <jaztyn72@gmail.com>
2020-06-01 13:12:01 +0800

    Smart Table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,44 @@ export class AppComponent {
   title = 'Directives';
   description = 'ngFor Implementation';
 
+  settings = {
+    columns: {
+      id: {
+        title: 'ID'
+      },
+      name: {
+        title: 'Full Name'
+      },
+      username: {
+        title: 'User Name'
+      },
+      email: {
+        title: 'Email'
+      }
+    }
+  };
+  
+  data = [
+    {
+      id: 1,
+      name: "test Graham",
+      username: "Bret",
+      email: "test@april.com"
+    },
+    {
+      id: 2,
+      name: "test Howell",
+      username: "test",
+      email: "test@gmail.tv"
+    },
+    {
+      id: 11,
+      name: "testDuBuque",
+      username: "test.Stanton",
+      email: "test@test.biz"
+    }
+  ];
+  
   showSecret = false;
   log = [];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
- 
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+
 @NgModule({
   declarations: [
     AppComponent, 
@@ -11,7 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    
+    Ng2SmartTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
