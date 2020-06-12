2a49f94a4113354a7c2505fe8421a306825b3935
TInjusan <jaztyn72@gmail.com>
2020-06-12 16:38:11 +0800

    Passing selected recipe data to recipe detail component with property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model'
+ 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes:Recipe [] =[
     new Recipe('Test 1', 'This is a test 1', 'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/16:9/w_1000,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'),
     new Recipe('Test 2', 'This is a test 2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ-TCn9a7UeRYA44PbgU6Zk8E-_z1YnvvmZ5xj6sz8q2ecaf42P&usqp=CAU')
@@ -15,5 +18,8 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
 
+  }
 }
