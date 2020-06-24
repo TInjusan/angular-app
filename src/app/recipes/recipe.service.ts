5b66bd5b6a0db70e05e0f586e09d17ce10cbd1e3
TInjusan <jaztyn72@gmail.com>
2020-06-24 21:41:23 +0800

    diplay ingredients in the recipe detail using services and dependency injection

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,27 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService{
   recipeSelected =  new EventEmitter<Recipe>();
 
   private  recipes:Recipe [] =[
-            new Recipe('Test 1', 'This is a test 1', 'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/16:9/w_1000,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'),
-            new Recipe('Test 2', 'This is a test 2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ-TCn9a7UeRYA44PbgU6Zk8E-_z1YnvvmZ5xj6sz8q2ecaf42P&usqp=CAU')
+            new Recipe('Schnitzel', 
+                       'This is a schnitzel', 
+                       'https://www.thespruceeats.com/thmb/oAls2Y49HemUbrDOUAdv14Mk3V8=/4288x2412/smart/filters:no_upscale()/wiener-schnitzel-recipe-1447089-Hero-5b587d6c46e0fb0071b0059d.jpg',
+                       [
+                           new Ingredient('Meat', 1),
+                           new Ingredient('French Fries', 20)
+                       ]),
+            new Recipe('Burger',
+                       'This is a Burger',
+                       'https://i2.wp.com/www.foodrepublic.com/wp-content/uploads/2012/03/033_FR11785.jpg?fit=1000%2C665&ssl=1', 
+                       [
+                         
+                            new Ingredient('Buns', 2),
+                            new Ingredient('Meat', 1)
+                         
+                       ])
             ];
 
   getRecipe(){
