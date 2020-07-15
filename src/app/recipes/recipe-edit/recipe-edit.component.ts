3a47e485e7ce765df7733149df7b419d2a90bac1
TInjusan <jaztyn72@gmail.com>
2020-07-15 16:22:50 +0800

    Implementing Delete and Cancel or clear functionality in recipe detail and edit

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
-import { Recipe } from '../recipe.model';
-
+ 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
@@ -14,7 +13,7 @@ export class RecipeEditComponent implements OnInit {
   editMode = false;
   recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService, private router: Router) { }
 
   ngOnInit(): void {
     console.log('the boggie'); 
@@ -40,6 +39,7 @@ export class RecipeEditComponent implements OnInit {
     else{
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+    this.onCancel();
   }
   private initForm(){
     
@@ -86,5 +86,7 @@ export class RecipeEditComponent implements OnInit {
       })
     )
   }
-  
+  onCancel(){
+    this.router.navigate(['../'], {relativeTo: this.route});
+  }
 }
