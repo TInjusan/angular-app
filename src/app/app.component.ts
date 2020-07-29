e51271209bd9547d907d785cfc334b0c21882144
TInjusan <jaztyn72@gmail.com>
2020-07-29 19:41:16 +0800

    Saving userdata in local storage and auto login using that saved userdata

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title ="Recipe App";
-  loadedFeature = 'recipe';
+export class AppComponent implements OnInit{
 
-  onNavigate(feature: string){
-     this.loadedFeature = feature;
+  constructor(private authService: AuthService){}
+
+  ngOnInit(){
+    this.authService.autoLogin();
   }
 }
