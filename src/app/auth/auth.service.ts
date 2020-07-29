31bce657673dc68e4259e26af2e8016abbfafc29
TInjusan <jaztyn72@gmail.com>
2020-07-29 20:41:27 +0800

    Implemeting auto log out based on a time out duration

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,105 +2,120 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Interface } from 'readline';
 import { catchError, tap } from 'rxjs/operators';
-import { throwError,  BehaviorSubject } from 'rxjs';
+import { throwError, BehaviorSubject } from 'rxjs';
 import { User } from './user.model';
 import { Router } from '@angular/router';
 
-export interface AuthResponseData{
+export interface AuthResponseData {
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
-    registered ?: boolean;
+    registered?: boolean;
 }
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class AuthService {
-    user =  new BehaviorSubject<User>(null);
+    user = new BehaviorSubject<User>(null);
+    private tokenExpirationTimer: any;
 
-    constructor(private http:  HttpClient, private router: Router){}
+    constructor(private http: HttpClient, private router: Router) { }
 
-    signUp(email: string, password: string){
-      return  this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDjcMosQvGASLG_sdrlfeY_VZd7p6xmcK4',
-        {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        }
-        ).pipe( catchError( this.handleError),
-        tap(resData => {
-           this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+    signUp(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDjcMosQvGASLG_sdrlfeY_VZd7p6xmcK4',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        ).pipe(catchError(this.handleError),
+            tap(resData => {
+                this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
 
-        }) );
+            }));
     }
 
-    login(email: string, password: string){
-        return  this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDjcMosQvGASLG_sdrlfeY_VZd7p6xmcK4',
-        {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        }
-        
-        ).pipe( catchError( this.handleError),
-        tap(resData => {
-           this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDjcMosQvGASLG_sdrlfeY_VZd7p6xmcK4',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
 
-        }) );
+        ).pipe(catchError(this.handleError),
+            tap(resData => {
+                this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+
+            }));
 
     }
-    autoLogin(){
-        const  userData: {
+    autoLogin() {
+        const userData: {
             email: string;
             id: string;
             _token: string;
             _tokenExpirationDate: string;
         } = JSON.parse(localStorage.getItem('userData'));
-        if(!userData){
+        if (!userData) {
             return;
         }
-        
-        const loadedUser = new User (userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate) );
-        
-        if(loadedUser.token){
+
+        const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
+        if (loadedUser.token) {
             this.user.next(loadedUser);
+            const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();   
+            this.autoLogOut(expirationDuration);
         }
     }
 
-    logout(){
+    logout() {
         this.user.next(null);
         this.router.navigate(['/auth']);
+        localStorage.removeItem('userData');
+        if (this.tokenExpirationTimer){
+            clearTimeout(this.tokenExpirationTimer);
+        }
     }
 
-    private handleAuthentication (email: string, userId: string, token: string, expiresIn: number){
+    autoLogOut(expirationDuration: number) {
+       this.tokenExpirationTimer = setTimeout(() => {
+            this.logout()
+            //You may put shorter time here like 2000 ms to check if it works
+        }, expirationDuration);
+
+    }
+    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
         const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
         const user = new User(email, userId, token, expirationDate);
         this.user.next(user);
+        this.autoLogOut(expiresIn * 1000);
         localStorage.setItem('userData', JSON.stringify(user));
 
     }
 
 
 
-    private handleError (errorResponse: HttpErrorResponse){
+    private handleError(errorResponse: HttpErrorResponse) {
         let errorMessage = 'An unknown error occured';
-        if (!errorResponse.error || !errorResponse.error.error){
-          return throwError(errorMessage);
+        if (!errorResponse.error || !errorResponse.error.error) {
+            return throwError(errorMessage);
         }
 
-        switch (errorResponse.error.error.message){
-          case 'EMAIL_EXISTS':
+        switch (errorResponse.error.error.message) {
+            case 'EMAIL_EXISTS':
                 errorMessage = 'This email already exists';
                 break;
-           case 'EMAIL_NOT_FOUND':
+            case 'EMAIL_NOT_FOUND':
                 errorMessage = 'This email doesn\'t exists';
                 break;
-           case 'INVALID_PASSWORD':
+            case 'INVALID_PASSWORD':
                 errorMessage = 'The password is incorrect';
                 break;
-        } 
+        }
         return throwError(errorMessage);
-     
+
     }
 }
