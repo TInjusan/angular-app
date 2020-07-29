a40379b24c1a2da992e5535308b3ae12416daf31
TInjusan <jaztyn72@gmail.com>
2020-07-29 18:18:39 +0800

    Getting the save data function working by attaching  token in interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,47 +1,43 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http'; 
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 import { map, tap, take, exhaust, exhaustMap } from 'rxjs/operators';
 import { AuthService } from '../auth/auth.service';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 
 export class DataStorageService {
 
-    constructor(private http: HttpClient, 
-                private recipeService: RecipeService,
-                private authService: AuthService){}
+    constructor(private http: HttpClient,
+        private recipeService: RecipeService,
+        private authService: AuthService) { }
 
-    storeRecipes(){
+    storeRecipes() {
         const recipes = this.recipeService.getRecipe();
         //Firebase uses put method to fetch all data and overwrite existing
-        return  this.http.put('https://my-first-app-2e9eb.firebaseio.com/recipes.json', recipes )
-        .subscribe(reponse =>{
-            console.log(reponse);
-        });
-    }
-    fetchRecipes(){
-
-     return this.authService.user.pipe(
-        take(1),
-        exhaustMap(user =>{
-            return this.http.get<Recipe[]>
-            ('https://my-first-app-2e9eb.firebaseio.com/recipes.json',
-                {
-                    params: new HttpParams().set('auth', user.token)
-                }
-            );            
-        }),  
-        map(recipes => {
-            return recipes.map(recipe => {
-                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+        return this.http.put('https://my-first-app-2e9eb.firebaseio.com/recipes.json', recipes)
+            .subscribe(reponse => {
+                console.log(reponse);
             });
-        }),
-        tap(recipes =>{
-            this.recipeService.setRecipes(recipes);
-         })
-     );    
-          
+    }
+    fetchRecipes() {
+
+
+        return this.http.get<Recipe[]>
+            ('https://my-first-app-2e9eb.firebaseio.com/recipes.json')            
+             .pipe(
+                map(recipes => {
+                    return recipes.map(recipe => {
+                        return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
+                    });
+                }),
+                tap(recipes => {
+                    this.recipeService.setRecipes(recipes);
+                })
+            );
+
+
+
     }
 }
