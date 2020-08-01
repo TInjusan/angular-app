c75ea2e3a58e0b66a850a18e850238165a083dfc
TInjusan <jaztyn72@gmail.com>
2020-08-01 22:16:33 +0800

    Implementing shared modules to avoid code redundancy

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -4,18 +4,18 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
 
     declarations: [ShoppingListComponent, ShoppingEditComponent ],
     imports: [
-        CommonModule,
         FormsModule,
+        SharedModule,
         RouterModule.forChild([
             {path: 'shopping-list', component: ShoppingListComponent}
         ])
-        
     ]
   
 })
